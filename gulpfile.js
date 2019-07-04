8b13dd51852929908672117a87bac62ab7db0722
Steve Hobbs <steve.hobbs.mail@gmail.com>
2019-07-04 16:48:05 +0100

    Removed Bootstrap dependency
    Also remove pre-commit hook, in favour of manual rebuild before raising
    PR

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,26 @@
-const { src, dest, series, parallel } = require('gulp');
-const rename = require('gulp-rename');
-const rimraf = require('rimraf');
-const sass = require('gulp-sass');
-const cleanCss = require('gulp-clean-css');
+const { src, dest, series, parallel } = require("gulp");
+const rename = require("gulp-rename");
+const rimraf = require("rimraf");
+const sass = require("gulp-sass");
+const cleanCss = require("gulp-clean-css");
 
-const outputDir = './dist';
-sass.compiler = require('node-sass');
+const outputDir = "./dist";
+sass.compiler = require("node-sass");
 
 /**
  * Cleans up the output directory
  */
 function clean(cb) {
   rimraf(outputDir, cb);
-};
-
-/**
- * JavaScript asset copy
- */
-function js() {
-  return src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
-    .pipe(rename('auth0-theme.min.js'))
-    .pipe(dest(`${outputDir}/js`));
-};
+}
 
 /**
  * SASS processing
  */
 function css() {
-  return src('./src/scss/entry.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename('auth0-theme.css'))
+  return src("./src/scss/entry.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(rename("auth0-theme.css"))
     .pipe(dest(`${outputDir}/css`));
 }
 
@@ -39,19 +30,9 @@ function css() {
 function minifyCss() {
   return src(`${outputDir}/css/*.css`)
     .pipe(cleanCss())
-    .pipe(rename('auth0-theme.min.css'))
+    .pipe(rename("auth0-theme.min.css"))
     .pipe(dest(`${outputDir}/css`));
 }
 
-function copyImages() {
-  return src('./src/images/**/*')
-    .pipe(dest(`${outputDir}/images`));
-}
-
 exports.clean = clean;
-exports.default = series(clean, 
-  parallel(
-    js, 
-    series(css, minifyCss),
-    copyImages
-  ));
+exports.default = series(clean, parallel(series(css, minifyCss)));
